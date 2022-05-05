5f9ee5c2cd0a20cf2e4d27574ac520cdfc0077b1
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-05-05 19:33:39 +0530

    Update AzureStorage3.tf

--- AzureStorage3.tf
+++ AzureStorage3.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example33" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "example39" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
