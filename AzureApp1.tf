bd60cfd2226cb20ffb63b39e278942240152e53e
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-06-21 17:53:48 +0530

    Update AzureApp1.tf

--- AzureApp1.tf
+++ AzureApp1.tf
@@ -3,5 +3,4 @@ resource "azurerm_app_service" "remediation_example11" {
     location            = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
     app_service_plan_id = azurerm_app_service_plan.example.id
-    https_only          = "false"
 }
