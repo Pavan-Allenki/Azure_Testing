0ef1ab190528d4c13fc7e9ff204f9e5d51f5fa5d
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-06-22 16:59:56 +0530

    Update AzureApp.tf

--- AzureApp.tf
+++ AzureApp.tf
@@ -1,4 +1,4 @@
-resource "azurerm_function_app" "remediation_example12" {
+resource "azurerm_function_app" "remediation_example12345" {
   name                = "example-function-app"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
